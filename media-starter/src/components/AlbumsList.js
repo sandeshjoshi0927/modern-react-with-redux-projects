9757fe735353299c7fca77ef02db8622b8265b9a
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2025-02-16 08:24:29 +0545

    albums api

--- media-starter/src/components/AlbumsList.js
+++ media-starter/src/components/AlbumsList.js
@@ -1,10 +1,15 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
 
 const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
@@ -24,7 +29,10 @@ const AlbumsList = ({ user }) => {
 
   return (
     <div>
-      <div>Albums for {user.name}</div>
+      <div>
+        Albums for {user.name}
+        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
       <div>{content}</div>
     </div>
   );
