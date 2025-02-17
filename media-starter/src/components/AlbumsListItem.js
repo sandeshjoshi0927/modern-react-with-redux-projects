6ef91c6a8f80b987d96af2155f5fbd94f15cd543
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2025-02-17 17:27:13 +0545

    albums done

---
+++ media-starter/src/components/AlbumsListItem.js
@@ -0,0 +1,33 @@
+import { GoTrashcan } from "react-icons/go";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import { useRemoveAlbumMutation } from "../store";
+import PhotosList from "./PhotosList";
+
+const AlbumsListItem = ({ album }) => {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <>
+      <Button
+        loading={results.isLoading}
+        className="mr-3 hover:bg-red-500 hover:text-white"
+        onClick={handleRemoveAlbum}
+      >
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </>
+  );
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      <PhotosList album={album} />
+    </ExpandablePanel>
+  );
+};
+
+export default AlbumsListItem;
--- media-starter/src/store/apis/albumsApi.js
+++ media-starter/src/store/apis/albumsApi.js
@@ -1,16 +1,33 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
+import { pause } from "../thunks/pause";
 
 const albumsApi = createApi({
   reducerPath: "albums",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3005",
+    fetchFn: async (...args) => {
+      //DEV ONLY
+      await pause(1000);
+      return fetch(...args);
+    },
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.id }];
+        },
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          return [{ type: "UsersAlbum", id: user.id }];
         },
         query: (user) => {
           return {
@@ -25,7 +42,11 @@ const albumsApi = createApi({
       }),
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbum", id: user.id });
+          return tags;
         },
         query: (user) => {
           return {
@@ -41,5 +62,9 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
